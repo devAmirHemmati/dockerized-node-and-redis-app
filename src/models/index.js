0172a1840ffbc9c01e665593da19f270e03007a0
Amir <dev.amirhemmati1382@gmail.com>
2021-12-24 01:40:33 +0330

    fix bug in Dockerfile and docker-compose.yml add delete featur for users

--- src/models/index.js
+++ src/models/index.js
@@ -6,7 +6,12 @@ class UserDB {
   }
 
   async createRedis() {
-    this.client = redis.createClient();
+    this.client = redis.createClient({
+      url:
+        process.env.NODE_ENV === 'production'
+          ? 'redis://redis_db'
+          : 'redis://localhost',
+    });
 
     this.client.on('connect', () => {
       console.log('connect with redis');
@@ -28,6 +33,27 @@ class UserDB {
     parsedUsers.push(data);
 
     await this.client.set('users', JSON.stringify(parsedUsers));
+  }
+
+  async deleteUser(data) {
+    const users = await this.client.get('users');
+    const parsedUsers = JSON.parse(users) || [];
+
+    if (!Array.isArray(parsedUsers)) return;
+
+    const findUser = parsedUsers.find(
+      (item) =>
+        item.firstName === data.firstName && item.lastName === data.lastName
+    );
+
+    if (typeof findUser === 'undefined') return;
+
+    const filterUsers = parsedUsers.filter(
+      (item) =>
+        item.firstName !== data.firstName || item.lastName !== data.lastName
+    );
+
+    await this.client.set('users', JSON.stringify(filterUsers));
   }
 }
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,16 @@
 const express = require('express');
-const { GetHomeController, PostHomeController } = require('./../controller');
+const {
+  GetHomeController,
+  PostHomeController,
+  DeleteHomeController,
+} = require('./../controller');
 
 const app = express.Router();
 
 app.get('/', GetHomeController);
 
 app.post('/', PostHomeController);
+
+app.post('/delete', DeleteHomeController);
 
 module.exports = app;
